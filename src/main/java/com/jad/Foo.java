62da946d90de14b2ad5eed3f3cd5bf718c1e5bf7
wh0amix <mougammadou.anas@gmail.com>
2025-11-21 15:50:15 +0100

    ajout attributs,constructeurs, méthodes getter et setter

---
+++ src/main/java/com/jad/Foo.java
@@ -0,0 +1,62 @@
+package com.jad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Foo {
+    // Attributs
+    private Bar bar;                        // Association simple (1)
+    private List<Baz> bazs;                 // Agrégation (*)
+    private Qux qux;                        // Composition (1)
+    private Corge corge;                    // Association bidirectionnelle (0..1)
+    private List<Grault> graults;           // Composition (*)
+
+    // Constructeur
+    public Foo(Bar bar) {
+        this.bar = bar;
+        this.qux = new Qux();               // COMPOSITION : créé ici
+        this.bazs = new ArrayList<>();
+        this.graults = new ArrayList<>();
+    }
+
+    // Getters
+    public Bar getBar() {
+        return bar;
+    }
+
+    public List<Baz> getBazs() {
+        return bazs;
+    }
+
+    public Qux getQux() {
+        return qux;
+    }
+
+    public Corge getCorge() {
+        return corge;
+    }
+
+    public List<Grault> getGraults() {
+        return graults;
+    }
+
+    // Setters basiques (sauf setCorge traité plus tard)
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+
+    public void setBazs(List<Baz> bazs) {
+        this.bazs = bazs;
+    }
+
+    public void setQux(Qux qux) {
+        this.qux = qux;
+    }
+
+    public void setGraults(List<Grault> graults) {
+        this.graults = graults;
+    }
+    public void addBaz(Baz baz) {
+        this.bazs.add(baz);
+    }
+}
